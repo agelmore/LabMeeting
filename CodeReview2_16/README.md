0dc175a592f7984acd013f7134d4735f4969167e
Amanda <agelmore@umich.edu>
2017-02-15 12:04:00 -0500

    added base code

--- CodeReview2_16/README.md
+++ CodeReview2_16/README.md
@@ -17,10 +17,17 @@ As an exercise in problem solving and using dplyr, we will split into groups to 
 
 3) Pat + Charlie + Kaitlin
 
+###Download repo
+
+~~~~
+git clone https://github.com/agelmore/LabMeeting.git
+~~~~
+
 
 ###File summary
 
 1) metadata 
+
 	+ Each independent sample from colonization study
 	+ Donors: 10 total; 6 human (2 are split into A and B experiments), 1 mouse, 1 soil
 	+ Eartags/cages: 61 total, 3-5 per cage, 1-2 cages per donor
@@ -28,22 +35,25 @@ As an exercise in problem solving and using dplyr, we will split into groups to 
 	+ Donor.id - sample id of the donor associated with that mouse
 
 2) functions
+
 	+ load_Libraries 
 	+ get_day1 and get_endpoints - input metadata data frame and eartag names, return vector of sample id for that mouse's last sample
 
 3) dist.col
+
 	+ Column formatted distance matrix
 	+ One line with thetaYC every sample comparison, no repeats
 	+ "label" and "comparison" are column headers with id names compared
 	+ 1372 * 1371 / 2 = 940506
 
 4) dist_to_endpoint.R
+
 	+ Bare R script to make plot
 
 
 ###Exercise:
 
-1) Calculate the average distance from donor to endpoint in each donor
+1) Calculate the **average distance from donor to endpoint in each donor**
 
 2) Calculate standard error  
 
--- CodeReview2_16/dist_to_endpoint.R
+++ CodeReview2_16/dist_to_endpoint.R
@@ -1,6 +1,7 @@
 ##Code to summarize some other beta diversity scripts. Creating a figure similar to Figure 2a in Baxter et al 2014. 
 
 #Calculate the following distances by thetaYC for each donor then summarize in dot plot with standard error
+
 # Inoculum vs D1
 # Inoculum vs endpoint
 # D1 vs endpoint
@@ -9,7 +10,6 @@
 setwd("/users/amanda/documents/Schloss/Lab meeting/LabMeeting/CodeReview2_16/")
 
 source('functions.R')
-
 
 load_Libraries(c("ggplot2","gridExtra","dplyr","tidyr"))
 
@@ -26,11 +26,41 @@ colnames(dist.col) <- c("label", "comparison", "thetayc")
 
 
 #Summarize data
-eartags <- unique(metadata$eartag_un[metadata$type=='sample'& metadata$treatment=='none' &metadata$strain=='C57Bl/6'])
-donors <- unique(metadata$donor[metadata$type=='sample'&metadata$treatment=='none'&metadata$strain=='C57Bl/6'])
+eartags <- unique(metadata$eartag_un[metadata$type=='sample'& metadata$treatment=='none' &metadata$strain=='C57Bl/6']) #vector of unique eartags
+donors <- unique(metadata$donor[metadata$type=='sample'&metadata$treatment=='none'&metadata$strain=='C57Bl/6']) #vector of unique donor names
+
+#Sample id vectors
 donor.id <- unique(metadata$donor.id[metadata$type=='sample'& metadata$treatment=='none' &metadata$strain=='C57Bl/6'])
 end.id <- get_endpoints(eartags, metadata)
-d1.id <- unique(metadata$id[metadata$])
 d1.id <- get_day1(eartags, metadata)
+
+#Calculate mean thetaYC distance and standard error between inoculum and endpoint in each donor
+
+
+
+
+mean.donor.endpoint <- 
+
+#Inoculum vs Day 1
+
+
+
+#Day 1 vs Endpoint
+
+#Plot distance in each donor
+
+donor.palette <- c("hotpink","red","orange3", "yellow3", "forestgreen", "blue", "purple","brown", "black", "skyblue3")
+
+change <- ggplot(mean.donor.endpoint, aes(x=, y=, color=)) +
+  ylab(expression(paste("Mean ", theta["YC"], " distance per donor", sep=" ")))+
+  xlab("") +
+  theme_bw() +
+  ylim(range(0,1))+
+  scale_color_manual(values=donor.palette)+
+  ggtitle("Community change over course of experiment")+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.position='none') 
+
+
+
 
 
