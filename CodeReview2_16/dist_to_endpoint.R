42e62c5fffc3602935ac8e112a3b9f2eb3656171
Amanda <agelmore@umich.edu>
2017-02-16 14:51:59 -0500

    added solution

--- CodeReview2_16/dist_to_endpoint.R
+++ CodeReview2_16/dist_to_endpoint.R
@@ -31,33 +31,45 @@ donor.id <- unique(metadata$donor.id[metadata$type=='sample'& metadata$treatment
 end.id <- get_endpoints(eartags, metadata)
 d1.id <- get_day1(eartags, metadata)
 
-#Calculate mean thetaYC distance and standard error between inoculum and endpoint in each donor
 
 
+##use dists file to subset to all distances to each donor
+dist.donors <- rbind(merge(metadata, dist.col, by.x=c("id", "donor.id"), by.y=c("comparison", "label")), merge(metadata, dist.col, by.x=c("id", "donor.id"), by.y=c("label", "comparison"))) #merge files to get donors to only their experiment. Have to rbind because donorid could be in right or left column of comparison
 
 
-mean.donor.endpoint <- 
+# Inoculum vs endpoint
+dist.end <- dist.donors[dist.donors$id %in% end.id,]
 
-#Inoculum vs Day 1
+mean.end.donor <- dist.end %>% 
+  group_by(donor) %>% 
+  summarise(mean.inoc.vs.end = mean(thetayc), se.inoc.vs.end =sd(thetayc)/sqrt(n())) %>%  
+  unite("mean.inoc.vs.end_se.inoc.vs.end", mean.inoc.vs.end,se.inoc.vs.end) %>% 
+  gather(key="calc_error", value, 2) %>% 
+  separate(calc_error, c("calc", "error"), "_") %>%  
+  separate(value, c("mean.value", "error.value"), "_")
 
 
-
-#Day 1 vs Endpoint
 
 #Plot distance in each donor
 
-donor.palette <- c("hotpink","red","orange3", "yellow3", "forestgreen", "blue", "purple","brown", "black", "skyblue3")
+dodge <- position_dodge(.5)
+donor.palette <- c("hotpink","red","orange3", "yellow3", "forestgreen", "blue", "purple","brown", "black", "lightblue")
 
-change <- ggplot(mean.donor.endpoint, aes(x=, y=, color=)) +
+#Convert numbers back to numeric
+mean.end.donor$mean.value <- as.numeric(mean.end.donor$mean.value)
+mean.end.donor$error.value <- as.numeric(mean.end.donor$error.value)
+
+
+ggplot(mean.end.donor, aes(x=calc, y=mean.value, color=donor)) +
+  geom_point(position=dodge, size=2) +
+  geom_errorbar(aes( ymax=mean.value+error.value, ymin=mean.value-error.value), position=dodge)+    
   ylab(expression(paste("Mean ", theta["YC"], " distance per donor", sep=" ")))+
   xlab("") +
   theme_bw() +
   ylim(range(0,1))+
   scale_color_manual(values=donor.palette)+
-  ggtitle("Community change over course of experiment")+
+  ggtitle("Change over time")+
   theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.position='none') 
-
-
 
 
 
